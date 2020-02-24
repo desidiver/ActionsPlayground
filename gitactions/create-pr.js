60bf3899c348d5f5d2a2f521565dbbe1a1b7365f
Nayan Savla <nayansavla@Gmail.com>
2020-02-24 15:08:51 -0800

    publishing actions
    commiting the actions.

---
+++ gitactions/create-pr.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  console.log('The event payload:');
+} catch (error) {
+  core.setFailed(error.message);
+}
