3025d638b71de24bea069f0be7c8a5fa35c415e4
Nayan Savla <nayansavla@Gmail.com>
2020-02-25 14:11:05 -0800

    another push

--- gitactions/create-pr/index.js
+++ gitactions/create-pr/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  console.log('The event payload ',core,github);
+  console.log('The event payload s ',core,github);
 } catch (error) {
   core.setFailed(error.message);
 }
